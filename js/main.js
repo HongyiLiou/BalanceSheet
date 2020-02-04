c513d09e4c26bf78ebf8dd7e174a75d7e3d555cb
HongYi Liou <t30034a7@digiwin.com>
2020-02-04 17:56:41 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,57 @@
+// date
+/** 年份選擇 */const selectYear = $('#year');
+/** 月份選擇 */const selectMonth = $('#month');
+/** 日期選擇 */const selectDay = $('#day');
+/** 取得時間 */const today = new Date();
+/** 取得今年 */const year = today.getFullYear();
+/** 取得今月 */const month = today.getMonth() + 1;
+/** 取得今日 */const day = today.getDate();
+console.log(year,month,day);
 
-// $.post('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', {
-//     name: 'Sasuke',
-//     age: 27    
-// }, function(e) {
-//     alert(e)
-// });
+window.onload = function() {
+    setYearMonthSelect();
+    
+}
 
-// var parameter = {
-//     url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=0', // 試算表網址
-//     name: 'hong',
-//     startRow: 1,
-//     startColumn: 1
-// }
-
-// $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter, function(data) {
-//     alert(data)
-// })
+/** 年月 */
+function setYearMonthSelect() {
+    // 年份設置
+    for(let i = year - 5; i <= year + 5; i++) {
+        const option = document.createElement('option');
+        option.innerHTML = i;
+        selectYear[0].appendChild(option);
+    }
 
 
-/** Get */
+    // 月份設置
+    for(let i = 1; i <= 12; i++) {
+        const option = document.createElement('option');
+        option.setAttribute('value', `${i}`);
+        option.innerHTML = `${i}月`;
+        selectMonth[0].appendChild(option);
+    }
+
+    // 日設置
+    onMonthChanged();
+
+    selectYear[0].value = year;
+    selectMonth[0].value = month;
+    selectDay[0].value = day;
+}
+
+/** 日 */
+function onMonthChanged() {
+    selectDay.empty();
+    // 日設置
+    for(let i = 1; i <= 31; i++) {
+        const option = document.createElement('option');
+        option.setAttribute('value', `${i}`);
+        option.innerHTML = `${i}日`;
+        selectDay[0].appendChild(option);
+    }
+}
+
+/** Get Functions */
 class getFunction {
     constructor() {
         console.log('getFunction is ready.');
@@ -27,12 +60,11 @@ class getFunction {
 }
 
 
-/** Post */
+/** Post Functions */
 class postFunction {
 
-    /** 用於計算目前 */itemAmount = 2;
-
-    /** 所有支出欄位 */itemInput1s = Array.from($('.itemInput1'));
+    /** 所有支出項目欄位 */itemInputs = Array.from($('.itemInput'));
+    /** 所有支出金額欄位 */amountInputs = Array.from($('.amountInput'));
 
     constructor() {
         console.log('postFunction is ready.');
@@ -45,31 +77,32 @@ class postFunction {
 
     /** 按鈕按鈕 */
     onClickSendBtn() {
-        // const data1 = [[`${this.grid1.val()}`, `${this.grid2.val()}`]]
-        const data1 = [];
-        const dataContianer1 = [];
-        this.itemInput1s.forEach(element => {
-            if (element.value) {
-                dataContianer1.push(`${element.value}`);
-            } else {
-                return;
+        /** 最後要送至後端的 項目data */const data1 = [];
+        /** 最後要送至後端的 金額data */const data2 = [];
+        /** 用來裝 itemInputs的值 */const dataContianer1 = [];
+        /** 用來裝 amountInputs的值 */const dataContianer2 = [];
+
+        for (let i = 0; i < this.itemInputs.length; i++) {
+            if (this.itemInputs[i].value && this.amountInputs[i].value) {
+                dataContianer1.push(`${this.itemInputs[i].value}`);
+                dataContianer2.push(`${this.amountInputs[i].value}`);
             }
-        });
+        }
         
         data1.push(dataContianer1);
-        console.log(data1);
+        data2.push(dataContianer2);
         
-        const parameter = {
-            url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=0',
-            name: '2020BalanceSheet_hong',
-            functionType: 'post',
-            data1: data1.toString(),
-            // insertType: 'bottom',
-            row1: data1.length,
-            column1: data1[0].length,
-            range: `2020BalanceSheet_hong!B2:B3`
-        };
-        $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
+        // const parameter = {
+        //     url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=0',
+        //     name: '2020BalanceSheet_hong',
+        //     functionType: 'post',
+        //     data1: data1.toString(),
+        //     // insertType: 'bottom',
+        //     row1: data1.length,
+        //     column1: data1[0].length,
+        //     range: `2020BalanceSheet_hong!B2:B3`
+        // };
+        // $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
     }
 }
 
