d2da7d968c9c591b3073bd6999c36e8f138ffb54
HongYi Liou <t30034a7@gmail.com>
2020-02-07 20:46:21 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-// // date
-// /** 年份選擇 */const selectYear = document.getElementById('year');
-// /** 月份選擇 */const selectMonth = document.getElementById('month');
-// /** 日期選擇 */const selectDay = document.getElementById('day');
-// /** 取得時間 */const today = new Date();
-// /** 取得今年 */const year = today.getFullYear();
-// /** 取得今月 */const month = today.getMonth() + 1;
-// /** 取得今日 */const day = today.getDate();
-// /** 根據月份取得日天數 */let days = new Date(year,month,0).getDate();
-// console.log(year,month,day);
+// import ('./balanceSheet.js');
+// date
+/** 年份選擇 */yearInput = document.querySelector('#year');
+/** 月份選擇 */monthInput = document.querySelector('#month');
+/** 日期選擇 */dayInput = document.querySelector('#day');// date
+/** 年份顯示 */year_show = document.querySelector('#year_show');
+/** 月份顯示 */month_show = document.querySelector('#month_show');
+/** 日期顯示 */day_show = document.querySelector('#day_show');
+/** 取得時間 */today = new Date();
+/** 取得今年 */year = today.getFullYear();
+/** 取得今月 */month = today.getMonth() + 1;
+/** 取得今日 */day = today.getDate();
+/** 根據月份取得日天數 */days = new Date(year, month, 0).getDate();
 
 window.onload = function() {
+    initialToday();
     // setYearMonthSelect();
 
     // console.log(selectYear.value, selectMonth.value, selectDay.value);
@@ -17,49 +21,121 @@ window.onload = function() {
     
 }
 
-/** 設置初始年月 */
-function setYearMonthSelect() {
-    // 年份設置
-    for(let i = 0; i <= 3; i++) {
-        const option = document.createElement('option');
-        option.setAttribute('value', `${i + 2020}`);
-        option.innerHTML = `${i + 2020}年`;
-        selectYear.appendChild(option);
-        // selectYear.options[i] = new Option(`${ i + 2020 }年`, `${ i + 2020 }`);
+/** 年份選擇 */
+function onYearClick() {
+    yearInput.style.pointerEvents = 'none';
+    const options = document.querySelector('.userInput_year .options');
+    for (let i = 0; i <= 0; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = `${ i + 2020 }年`;
+        li.addEventListener('click', () => {
+            yearInput.value = i + 2020;
+            year_show.value = `${yearInput.value} 年`;
+            li.removeEventListener('click', onYearClick);
+            options.innerHTML = '';
+            yearInput.style.pointerEvents = 'auto';
+        });
+        options.appendChild(li);
     }
-
-
-    // 月份設置
-    for(let i = 1; i <= 12; i++) {
-        const option = document.createElement('option');
-        option.setAttribute('value', `${i}`);
-        option.innerHTML = `${i}月`;
-        selectMonth.appendChild(option);
-    }
-
-    selectYear.value = year;
-    selectMonth.value = month;
-    
-    // 日設置
-    onMonthChanged();
-    
-    selectDay.value = day;
 }
 
-/** 日 */
-function onMonthChanged() {
-    selectDay.innerHTML = 'none';
-    
-    days = new Date(year, selectMonth.value, 0).getDate();
+/** 月份選擇 */
+function onMonthClick() {
+    monthInput.style.pointerEvents = 'none';
+    const options = document.querySelector('.userInput_month .options');
+    for (let i = 1; i <= 12; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = `${i}月`;
+        li.addEventListener('click', () => {
+            monthInput.value = i;
+            month_show.value = `${monthInput.value} 月`;
+            li.removeEventListener('click', onMonthClick);
+            options.innerHTML = '';
+            monthInput.style.pointerEvents = 'auto';
+        });
+        options.appendChild(li);
+    }
+
+}
+
+/** 日選擇 */
+function onDayClick() {
+    dayInput.style.pointerEvents = 'none';
+    days = new Date(yearInput.value, monthInput.value, 0).getDate();
+    const options = document.querySelector('.userInput_day .options');
     // 日設置
     for(let i = 1; i <= days; i++) {
-        const option = document.createElement('option');
-        option.setAttribute('value', `${i}`);
-        option.innerHTML = `${i}日`;
-        selectDay.appendChild(option);
+        const li = document.createElement('li');
+        li.innerHTML = `${i}日`;
+        li.addEventListener('click', () => {
+            dayInput.value = i;
+            day_show.value = `${dayInput.value} 日`;
+            console.log(`dayInput.value`, dayInput.value);
+            options.innerHTML = '';
+            dayInput.style.pointerEvents = 'auto';
+        });
+        options.appendChild(li);
     }
-    
+
 }
+
+/** 初始化日期 */
+function initialToday() {
+    yearInput.value = year;
+    monthInput.value = month;
+    dayInput.value = day;
+    year_show.value = `${year} 年`;
+    month_show.value = `${month} 月`;
+    day_show.value = `${day} 日`;
+}
+
+
+
+
+
+// /** 設置初始年月 */
+// function setYearMonthSelect() {
+//     // 年份設置
+//     for(let i = 0; i <= 3; i++) {
+//         const option = document.createElement('option');
+//         option.setAttribute('value', `${i + 2020}`);
+//         option.innerHTML = `${i + 2020}年`;
+//         selectYear.appendChild(option);
+//         // selectYear.options[i] = new Option(`${ i + 2020 }年`, `${ i + 2020 }`);
+//     }
+
+
+//     // 月份設置
+//     for(let i = 1; i <= 12; i++) {
+//         const option = document.createElement('option');
+//         option.setAttribute('value', `${i}`);
+//         option.innerHTML = `${i}月`;
+//         selectMonth.appendChild(option);
+//     }
+
+//     selectYear.value = year;
+//     selectMonth.value = month;
+    
+//     // 日設置
+//     onMonthChanged();
+    
+//     selectDay.value = day;
+// }
+
+// /** 日 */
+// function onMonthChanged() {
+//     selectDay.innerHTML = 'none';
+    
+//     days = new Date(year, selectMonth.value, 0).getDate();
+//     // 日設置
+//     for(let i = 1; i <= days; i++) {
+//         const option = document.createElement('option');
+//         option.setAttribute('value', `${i}`);
+//         option.innerHTML = `${i}日`;
+//         selectDay.appendChild(option);
+//     }
+    
+// }
 
 /** 重置按鈕 */
 function onClickResetBtn() {
@@ -79,103 +155,96 @@ class getFunction {
 
 
 /** Post Functions */
-class postFunction {
 
-    /** 所有收支項目欄位 */itemInputs = Array.from($('.itemInput'));
-    /** 所有收支金額欄位 */amountInputs = Array.from($('.amountInput'));
+/** 所有收支項目欄位 */itemInputs = Array.from($('.itemInput'));
+/** 所有收支金額欄位 */amountInputs = Array.from($('.amountInput'));
 
-    constructor() {
-        console.log('postFunction is ready.');
-        
-    }
 
-    /** 上傳支出按鈕 */
-    onClickSendBtn() {
-        this.sendBalanceSheet();
-        this.sendBalanceSheetDetail();
-    }
+/** 上傳支出按鈕 */
+function onClickSendBtn() {
+    sendBalanceSheet();
+    sendBalanceSheetDetail();
+}
 
-    /** 更新收支表 */
-    sendBalanceSheet() {
-        /** 最後要送至後端的 收支data */const data = [];
-        /** amountInputs總和的值 */let dataContainer = 0;
-        /** 收支型態 */const type = $('.type');
+/** 更新收支表 */
+function sendBalanceSheet() {
+    /** 最後要送至後端的 收支data */const data = [];
+    /** amountInputs總和的值 */let dataContainer = 0;
+    /** 收支型態 */const type = $('.type');
 
-        for (let i = 0; i < this.amountInputs.length; i++) {
-            if (this.itemInputs[i].value && this.amountInputs[i].value) {
-                let amount = this.amountInputs[i].value;
+    for (let i = 0; i < amountInputs.length; i++) {
+        if (itemInputs[i].value && amountInputs[i].value) {
+            let amount = amountInputs[i].value;
 
-                if (type[i].value === 'expenditure' && (this.amountInputs[i].value).toString().indexOf('-') === -1 ) {
-                    amount = -(this.amountInputs[i].value);
-                }
-
-                dataContainer += Number(amount);
+            if (type[i].value === 'expenditure' && (amountInputs[i].value).toString().indexOf('-') === -1 ) {
+                amount = -(amountInputs[i].value);
             }
+
+            dataContainer += Number(amount);
         }
-
-        if (dataContainer !== 0) {
-            data.push(dataContainer);
-        }
-        
-        
-        const parameter = {
-            url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=0',
-            name: '2020BalanceSheet_hong',
-            functionType: 'post',
-            dataType: 'balanceSheet',
-            data: data.toString(),
-            month: Number(userInput.monthInput.value) + 1,
-            day: Number(userInput.dayInput.value) + 1,
-        };
-
-        console.log(parameter);
-        
-
-        $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
-
     }
 
-    /** 更新收支表 Detail */
-    sendBalanceSheetDetail() {
-        /** 最後要送至後端的 項目data */const data1 = [];
-        /** 最後要送至後端的 金額data */const data2 = [];
-        /** 用來裝 itemInputs的值 */const dataContianer1 = [];
-        /** 用來裝 amountInputs的值 */const dataContianer2 = [];
-        /** 收支型態 */const type = $('.type');
+    if (dataContainer !== 0) {
+        data.push(dataContainer);
+    }
+    
+    
+    const parameter = {
+        url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=0',
+        name: '2020BalanceSheet_hong',
+        functionType: 'post',
+        dataType: 'balanceSheet',
+        data: data.toString(),
+        month: Number(monthInput.value) + 1,
+        day: Number(dayInput.value) + 1,
+    };
 
-        for (let i = 0; i < this.itemInputs.length; i++) {
-            if (this.itemInputs[i].value && this.amountInputs[i].value) {
-                const item = this.itemInputs[i].value;
-                let amount = this.amountInputs[i].value;
+    console.log(parameter);
+    
 
-                if (type[i].value === 'expenditure' && (this.amountInputs[i].value).toString().indexOf('-') === -1 ) {
-                    amount = -(this.amountInputs[i].value);
-                }
+    $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
 
-                dataContianer1.push(item);
-                dataContianer2.push(amount.toString());
+}
+
+/** 更新收支表 Detail */
+function sendBalanceSheetDetail() {
+    /** 最後要送至後端的 項目data */const data1 = [];
+    /** 最後要送至後端的 金額data */const data2 = [];
+    /** 用來裝 itemInputs的值 */const dataContianer1 = [];
+    /** 用來裝 amountInputs的值 */const dataContianer2 = [];
+    /** 收支型態 */const type = $('.type');
+
+    for (let i = 0; i < itemInputs.length; i++) {
+        if (itemInputs[i].value && amountInputs[i].value) {
+            const item = itemInputs[i].value;
+            let amount = amountInputs[i].value;
+
+            if (type[i].value === 'expenditure' && (amountInputs[i].value).toString().indexOf('-') === -1 ) {
+                amount = -(amountInputs[i].value);
             }
+
+            dataContianer1.push(item);
+            dataContianer2.push(amount.toString());
         }
-        
-        data1.push(dataContianer1);
-        data2.push(dataContianer2);
-        
-        const parameter = {
-            url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=1948153821',
-            name: '2020BalanceSheetDetail_hong',
-            functionType: 'post',
-            dataType: 'balanceSheetDetail',
-            data1: data1.toString(),
-            data2: data2.toString(),
-            month: Number(userInput.monthInput.value) * 2,
-            day: Number(userInput.dayInput.value) + 1,
-        };
-
-        $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
-
     }
+    
+    data1.push(dataContianer1);
+    data2.push(dataContianer2);
+    
+    const parameter = {
+        url: 'https://docs.google.com/spreadsheets/d/1VCzkXIRBMjF9iv0Ca89xBLIzTSbYHMvXxxGq6lceusk/edit#gid=1948153821',
+        name: '2020BalanceSheetDetail_hong',
+        functionType: 'post',
+        dataType: 'balanceSheetDetail',
+        data1: data1.toString(),
+        data2: data2.toString(),
+        month: Number(monthInput.value) * 2,
+        day: Number(dayInput.value) + 1,
+    };
+
+    $.get('https://script.google.com/macros/s/AKfycbwC9bl6xw2PIbL6mF0ojN1RqokP_43JtxurpA2839FP80Ih2l19/exec', parameter);
+
 }
 
 
-const postFunction1 = new postFunction();
 // const getFunction1 = new getFunction();
