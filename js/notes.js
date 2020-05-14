e14bb0b17943821c525f5ba298d1078d9eac14a8
HongYi Liou <t30034a7@digiwin.com>
2020-05-14 17:49:11 +0800

    update

--- js/notes.js
+++ js/notes.js
@@ -2,13 +2,34 @@
 
 function onNoteActive() {
     const noteBooks = document.querySelectorAll('.notesPageBox .noteList ul li .book');
+    const noteList = document.querySelector('.notesPageBox .noteList');
+    const editNoteArea = document.querySelector('.notesPageBox .editNoteArea');
 
-    noteBooks.forEach(book => {
+    editNoteArea.classList.add('hide');
+
+    noteBooks.forEach((book, i) => {
         book.addEventListener('click', () => {
-            noteBooks.forEach(x => {
-                x.classList.remove('active');
+            console.log(123);
+            
+            noteBooks.forEach((x, j) => {
+                if (j !== i && x.classList.contains('active')) {
+                    x.classList.remove('active');
+                    x.classList.add('back');
+                    setTimeout(() => {
+                        x.classList.remove('back');
+                    }, 400);
+                }
             });
+
             book.classList.add('active');
+            setTimeout(() => {
+                noteList.classList.add('hide');
+                setTimeout(() => {
+                    noteList.style.display = 'none';
+                    editNoteArea.style.display = 'block';
+                    editNoteArea.classList.remove('hide');
+                }, 500)
+            }, 1000)
         });
     })
 }
