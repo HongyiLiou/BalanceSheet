ea14a46d385f2e1db6078673867c8eb142755f9e
HongYi Liou <t30034a7@gmail.com>
2020-05-23 21:21:44 +0800

    update

--- js/music.js
+++ js/music.js
@@ -8,11 +8,9 @@ let youTubePlayer;
  * @param {string}} videoId 
  */
 function onYouTubeIframeAPIReady(videoId) {
-    const album = document.querySelector('.musicPageBox .albumList .album');
     const ctrlq = document.getElementById("youtube-audio");
     ctrlq.innerHTML = '<div id="youtube-player"></div>';
     ctrlq.style.cssText = 'display:none';
-    album.classList.add('active');
 
     youTubePlayer = new YT.Player('youtube-player', {
         height: '0',
@@ -35,20 +33,44 @@ function onYouTubeIframeAPIReady(videoId) {
 
 /** 播放音樂 */
 function playYouTubePlayer() {
+    const album = document.querySelector('.musicPageBox .albumList .album');
+    const pauseBtn = document.querySelector('.musicPageBox .controler .buttons .pause');
+    const playBtn = document.querySelector('.musicPageBox .controler .buttons .play');
+    playBtn.style.display = 'none';
+    pauseBtn.style.display = 'block';
+    album.classList.remove('paused');
+    album.classList.add('active');
     setTimeout(() => {
         youTubePlayer.playVideo();
-    }, 800);
+    }, 1000);
 }
 
 
 /** 暫停播放音樂 */
 function pauseYouTubePlayer() {
+    const album = document.querySelector('.musicPageBox .albumList .album');
+    const pauseBtn = document.querySelector('.musicPageBox .controler .buttons .pause');
+    const playBtn = document.querySelector('.musicPageBox .controler .buttons .play');
+    pauseBtn.style.display = 'none';
+    playBtn.style.display = 'block';
+    album.classList.add('paused');
     youTubePlayer.pauseVideo();
 }
 
 
 /** 停止播放音樂 */
 function stopYouTubePlayer() {
+    const albums = document.querySelectorAll('.musicPageBox .albumList .album');
+    const pauseBtn = document.querySelector('.musicPageBox .controler .buttons .pause');
+    const playBtn = document.querySelector('.musicPageBox .controler .buttons .play');
+    pauseBtn.style.display = 'none';
+    playBtn.style.display = 'block';
     youTubePlayer.stopVideo();
+
+    albums.forEach(album => {
+        album.classList.remove('paused', 'active');
+    });
+
+    onYouTubeIframeAPIReady('LHZXT6813VE');
 }
 
