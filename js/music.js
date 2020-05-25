fbb783b345ae5603bcd5040a1a2bef95497e5c9f
HongYi Liou <t30034a7@digiwin.com>
2020-05-25 15:13:08 +0800

    update

--- js/music.js
+++ js/music.js
@@ -3,6 +3,24 @@
 
 /** YouTubePlayer */
 let youTubePlayer;
+/** YouTubePlayerSetting */
+let youTubePlayerSetting = {
+    height: '0',
+    width: '0',
+    videoId: '',
+    // videoId: 'LHZXT6813VE',
+    playerVars: {
+        loop: 1,
+        start: 1,
+        playlist: '',
+        enablejsapi: 1
+    },
+    events: {
+        'onReady': function() {
+            youTubePlayer.setPlaybackQuality("small");
+        }
+    }
+}
 /**
  * 重置播放器
  * @param {string}} videoId 
@@ -12,22 +30,12 @@ function onYouTubeIframeAPIReady(videoId) {
     ctrlq.innerHTML = '<div id="youtube-player"></div>';
     ctrlq.style.cssText = 'display:none';
 
-    youTubePlayer = new YT.Player('youtube-player', {
-        height: '0',
-        width: '0',
-        // videoId: ctrlq.dataset.video,
-        videoId: videoId,
-        // videoId: 'LHZXT6813VE',
-        playerVars: {
-        loop: 1,
-        start: 1
-        },
-        events: {
-            'onReady': function() {
-                youTubePlayer.setPlaybackQuality("small");
-            }
-        }
-    });
+    if (videoId) {
+        youTubePlayerSetting.videoId = videoId;
+        youTubePlayerSetting.playerVars.playlist = videoId;
+    }
+
+    youTubePlayer = new YT.Player('youtube-player', youTubePlayerSetting);
 }
 
 
@@ -55,6 +63,7 @@ function pauseYouTubePlayer() {
     playBtn.style.display = 'block';
     album.classList.add('paused');
     youTubePlayer.pauseVideo();
+    getVolumeYouTubePlayer();
 }
 
 
@@ -74,3 +83,29 @@ function stopYouTubePlayer() {
     onYouTubeIframeAPIReady('LHZXT6813VE');
 }
 
+
+/** 靜音 */
+function muteYouTubePlayer() {
+    const muteBtn = document.querySelector('.musicPageBox .controler .buttons .mute');
+    const unMuteBtn = document.querySelector('.musicPageBox .controler .buttons .unMute');
+    muteBtn.style.display = 'block';
+    unMuteBtn.style.display = 'none';
+    youTubePlayer.mute();
+}
+
+
+/** 取消靜音 */
+function unMuteYouTubePlayer() {
+    const muteBtn = document.querySelector('.musicPageBox .controler .buttons .mute');
+    const unMuteBtn = document.querySelector('.musicPageBox .controler .buttons .unMute');
+    muteBtn.style.display = 'none';
+    unMuteBtn.style.display = 'block';
+    youTubePlayer.unMute();
+}
+
+
+function getVolumeYouTubePlayer() {
+    const volume = youTubePlayer.getVolume();
+    console.log('音量音量', volume);
+    
+}
