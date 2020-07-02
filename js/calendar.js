804f815f54214bec9b7c74c748a136b4f51417f8
HongYi Liou <t30034a7@digiwin.com>
2020-07-02 10:31:33 +0800

    add calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,22 @@
 console.log('Calendar Page is ready.');
+
+/** client_id */
+// 778859427056-10vjmc69bmoco4ihp23te7vn08pj7o4f.apps.googleusercontent.com
+
+
+/** api key */
+// AIzaSyDyOOtKCJxiXo6oKV0eZv2_0E8B0wpTaUI
+
+const credentialsJSON = {
+    web: {
+        "client_id": "778859427056-10vjmc69bmoco4ihp23te7vn08pj7o4f.apps.googleusercontent.com",
+        "project_id": "quickstart-1593654771135",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "P5egwr0waQ7IIP5lcScgz4lo",
+        "javascript_origins": [
+            "http://localhost:8000"
+        ]
+    }
+};
